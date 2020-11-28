bf1da058a33d3f6d532db6a25395ad281ed193a0
jjayakumarDev <x20154810@student.ncirl.ie>
2020-11-28 16:14:27 +0000

    reverted component scan

--- src/main/java/com/cvbuilder/api/CVBuilderAPI.java
+++ src/main/java/com/cvbuilder/api/CVBuilderAPI.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages="com.cvbuilder.api")
+@ComponentScan("com.cvbuilder")
 @EntityScan("com.cvbuilder")
 @EnableJpaRepositories("com.cvbuilder.dao")
 public class CVBuilderAPI extends SpringBootServletInitializer{
