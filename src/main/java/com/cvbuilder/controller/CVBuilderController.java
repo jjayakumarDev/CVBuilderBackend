18c0aa9d82042137826d962284e6f3cfb8e06fc5
jjayakumarDev <73401040+jjayakumarDev@users.noreply.github.com>
2020-12-04 21:20:32 +0000

    Update CVBuilderController.java
    added s3 endpoing

--- src/main/java/com/cvbuilder/controller/CVBuilderController.java
+++ src/main/java/com/cvbuilder/controller/CVBuilderController.java
@@ -21,7 +21,7 @@ import com.cvbuilder.dao.LoginRepository;
 import com.cvbuilder.entity.CVInfo;
 import com.cvbuilder.entity.Login;
 
-@CrossOrigin(origins = "https://master.d301d2fm43kvix.amplifyapp.com")
+@CrossOrigin(origins = "http://cvbuilder-react.s3-website-us-east-1.amazonaws.com")
 @RestController
 public class CVBuilderController {
 	
